8608c69eb370a19d11979488212e45448728c65a
Ivona Kupanova <ivona@halicea.com>
2023-09-12 15:31:16 +0200

    Deleted some stuff

--- Randomizer-sample/Program.cs
+++ Randomizer-sample/Program.cs
@@ -1,19 +1,34 @@
 using Azure.Monitor.OpenTelemetry.AspNetCore;
 using InstantAPIs;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using OTM_sample.DbContext;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<ApplicationDbContext>(opt => 
+builder.Services.AddDbContext<ApplicationDbContext>(opt =>
     opt.UseNpgsql(builder.Configuration["Database:ConnectionString"]!));
-builder.Services.AddOpenTelemetry().UseAzureMonitor(options =>
+builder.Services.AddSwaggerGen(options =>
 {
-    options.ConnectionString = builder.Configuration["ApplicationInsights:ConnectionString"];
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "The quote of the day!",
+        Description = ""
+    });
 });
 builder.Services.AddInstantAPIs(options => options.EnableSwagger = EnableSwagger.Always);
 
 var app = builder.Build();
+
+app.MapGet("/api/quote-of-the-day", (ApplicationDbContext dbContext) =>
+{
+    var random = new Random();
+    if (!dbContext.Quotes.Any()) return Results.NotFound();
+    var index = random.Next(0, dbContext.Quotes.Count() - 1);
+    var quote = dbContext.Quotes.ToList().ElementAt(index);
+    return Results.Ok("Quote of the day is : " + quote.Text);
+
+});
 
 app.MapInstantAPIs<ApplicationDbContext>();
 
@@ -24,4 +39,3 @@ using (var serviceScope = app.Services.GetRequiredService<IServiceScopeFactory>(
 }
 
 app.Run();
-
